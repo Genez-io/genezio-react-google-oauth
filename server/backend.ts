155969bb07b8127b7fde06bdd8a6839eb08311d3
Bogdan Vlad <bogdan@genez.io>
2024-02-20 09:09:45 +0200

    Make backend function async.

--- server/backend.ts
+++ server/backend.ts
@@ -6,7 +6,7 @@ export class BackendService {
   readonly secret = "Capybaras are AWESOME! Shhh... don't tell the cats!";
 
   @GenezioMethod()
-  getSecret(): string {
+  async getSecret(): Promise<string> {
     return this.secret;
   }
 }
